5f0590df128419a1f40b97fc111ebe683aea5a25
Federico Ruggeri <federugg26@gmail.com>
2023-11-27 14:45:46 +0100

    tmp update to command runners

--- cinnamon_generic/components/command_runner.py
+++ cinnamon_generic/components/command_runner.py
@@ -1,12 +1,10 @@
-from argparse import ArgumentParser
 from typing import Callable
 
 from cinnamon_core.core.component import Component
 from cinnamon_core.core.configuration import C
-from cinnamon_core.core.registry import Registry, RegistrationKey, NotRegisteredException
-from cinnamon_core.utility import logging_utility
 
 
+# TODO: update runners
 class ComponentRunner(Component):
 
     def parse_config(
@@ -18,22 +16,7 @@ class ComponentRunner(Component):
     def run(
             self,
     ):
-        parser = ArgumentParser()
-        parser.add_argument('--name', '-n', nargs='?', const=None, type=str)
-        parser.add_argument('--tags', '-t', nargs='+', const=None, type=str)
-        parser.add_argument('--namespace', '-ns', nargs='?', const=None, type=str)
-        args = parser.parse_args()
-
-        replace_key = RegistrationKey(name=args.name,
-                                      tags=set(args.tags) if args.tags is not None else args.tags,
-                                      namespace=args.namespace)
-        try:
-            replace_config = Registry.build_configuration_from_key(registration_key=replace_key)
-            return self.parse_config(replace_config)
-        except NotRegisteredException:
-            logging_utility.logger.warning(f'Could not find registration key {replace_key}. '
-                                           f'The runner will ignore the submitted registration key.')
-            return self.parse_config(self.config)
+        return self.parse_config(self.config)
 
 
 class MultipleComponentRunner(ComponentRunner):
